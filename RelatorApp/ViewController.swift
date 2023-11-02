efac83c06e5ddc7a5aaae6842dd37215523ff880
Chandika10 <karanamc7895@uhcl.edu>
2023-11-02 04:24:48 -0500

    Create project

---
+++ RelatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RelatorApp
+//
+//  Created by Chandika Karanam on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
