64ddc63c3143b8e2fc9dae6fc95a991408778691
Vadlamudiv4881 <vadlamudiv4881@uhcl.edu>
2023-11-02 12:40:01 -0500

    Conflicts solve

--- RelatorApp/Controllers/ExploreVC.swift
+++ RelatorApp/Controllers/ExploreVC.swift
@@ -7,14 +7,40 @@
 
 import UIKit
 
-class ExploreVC: UIViewController {
+class ExploreVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var Numguests: UITextField!
+    @IBOutlet weak var datefield: UITextField!
+    @IBOutlet weak var Propertysearchbar: UISearchBar!
+   
+    
+    @IBOutlet weak var ExploreCollectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Numguests.delegate = self
+        Numguests.becomeFirstResponder()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        Numguests.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "ExploreCell", for: indexPath) as! ExploreCell
+        return cell1
+        
+    }
 
     /*
     // MARK: - Navigation
