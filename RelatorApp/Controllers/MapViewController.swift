be3b3df3c4c81af9b9c924655d526c527e218087
Vadlamudiv4881 <vadlamudiv4881@uhcl.edu>
2023-11-02 12:25:33 -0500

    Created Map View using Corelocation and MapKit

--- RelatorApp/Controllers/MapViewController.swift
+++ RelatorApp/Controllers/MapViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class MapViewController: UIViewController {
 
+   
+    @IBOutlet weak var mapview: MKMapView!
+    
+    
+    var city : String? = "Delhi"
+    var annotation : MKPointAnnotation!
+    
+    func annotation(coordinate : CLLocationCoordinate2D, title: String!, subtitle : String!)
+    {
+        annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = title
+        annotation.subtitle = subtitle
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        // Initialize a geocoder
+               let geocoder = CLGeocoder()
 
-        // Do any additional setup after loading the view.
-    }
-    
+               // Geocode the city name to get its coordinates
+               geocoder.geocodeAddressString(city ?? "") { (placemarks, error) in
+                   if let error = error {
+                       print("Geocoding error: \(error.localizedDescription)")
+                   } else if let placemark = placemarks?.first {
+                 
+                       let coordinate = placemark.location?.coordinate ?? CLLocationCoordinate2D(latitude: 0, longitude: 0)
+                       let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+                       self.mapview.setRegion(region, animated: true)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+                       let annotation = MKPointAnnotation()
+                       annotation.coordinate = coordinate
+                       annotation.title = self.city
+                       self.mapview.addAnnotation(annotation)
+                   }
+               }
+           }
 
 }
