383c1dfaaf94d34df0c99b7a2a3d9106205cce6b
gajulapallik0777 <gajulapallik0777@uhcl.edu>
2023-12-07 11:15:59 -0600

    Created modal class

--- RelatorApp/Modal/StructModals.swift
+++ RelatorApp/Modal/StructModals.swift
@@ -5,4 +5,56 @@
 //  Created by Pavan Pullugundla on 11/2/23.
 //
 
+
 import Foundation
+struct SearchStruct: Codable
+{
+    let status : Bool?
+    let message : String?
+    let timestamp : Int?
+    let data : [searchdata]?
+    struct  searchdata  : Codable {
+        let price : String?
+        let publicAddress : String?
+        let city : String?
+        let title : String?
+        let avgRatingLocalized : String?
+        let roomType: String?
+        let spaceType : String?
+        let images : [String]?
+        
+    
+        let location : Location?
+        
+        struct Location: Codable {
+            let mapBoundsHint: MapBoundsHint?
+            struct MapBoundsHint: Codable {
+                let northeast: Coordinate?
+                let southwest: Coordinate?
+                struct Coordinate: Codable {
+                    let latitude: Double?
+                    let longitude: Double?
+                }
+            }
+            
+        }
+    }
+    
+    
+    
+}
+
+
+struct PlaceStruct: Codable
+{
+    let status : Bool?
+    let message : String?
+    let timestamp : Int?
+    let data : [Placedata]?
+    struct  Placedata  : Codable {
+        let id: String?
+        let location_name : String?
+        let country_code : String?
+        
+    }
+}
